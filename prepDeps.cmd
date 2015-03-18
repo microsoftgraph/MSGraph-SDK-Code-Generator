88646f1e96055036a6e85701ccc19265ba0d5611
Josh Gavant <josh.gavant@microsoft.com>
2015-03-18 11:15:01 -0700

    Remove prepDeps.cmd

--- prepDeps.cmd
+++
@@ -1,7 +0,0 @@
-git clone https://github.com/microsoft/vipr %TEMP%\vipr
-cmd /C %TEMP%\vipr\build.cmd
-mkdir .\dependencies
-copy /Y %TEMP%\vipr\src\Core\Vipr\bin\Debug\Vipr.exe .\dependencies\Vipr.exe
-copy /Y %TEMP%\vipr\src\Core\Vipr\bin\Debug\Vipr.Core.dll .\dependencies\Vipr.Core.dll
-copy /Y %TEMP%\vipr\src\Core\Vipr\bin\Debug\ODataReader.v4.dll .\dependencies\ODataReader.v4.dll
-rmdir /S /Q %TEMP%\vipr
