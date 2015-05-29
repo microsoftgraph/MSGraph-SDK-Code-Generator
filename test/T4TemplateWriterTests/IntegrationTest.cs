c3e7b3ec58550a35d3f28fd387a46b3483a8581f
joshgav <josh.gavant@outlook.com>
2015-05-29 14:58:19 -0700

    simple int test for template writer

---
+++ test/T4TemplateWriterTests/IntegrationTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vipr;
+
+namespace T4TemplateWriterTests {
+    class IntegrationTest {
+        private Vipr.Bootstrapper bootstrapper;
+
+        private String inputFile  = @"http://services.odata.org/V4/OData/OData.svc/$metadata";
+        private String readerName = @"--reader=Vipr.Reader.OData.v4";
+        private String writerName = @"--writer=T4TemplateWriter";
+        private String outputPath = @"--outputPath=.\output";
+
+        public IntegrationTest() {
+
+             bootstrapper = new Vipr.Bootstrapper();
+        }
+
+        public void Run() {
+            bootstrapper.Start(new String[] { inputFile, readerName, writerName, outputPath });
+            Console.ReadKey();
+        }
+    }
+}
---
+++ test/T4TemplateWriterTests/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T4TemplateWriterTests {
+    static class Program {
+        static public void Main(String[] args) {
+            new IntegrationTest().Run();
+        }
+    }
+}
