aefbaa1d3be10e6dfffd3c5bf4e6bd12756c5c21
Anahi Hojnadel <anahi.hojnadel@gmail.com>
2015-06-26 10:51:06 -0300

    Updated templates for ObjC

--- src/T4TemplateWriter/Helpers/ObjectiveC/PropertyHelper.cs
+++ src/T4TemplateWriter/Helpers/ObjectiveC/PropertyHelper.cs
@@ -6,123 +6,128 @@ using Vipr.Core.CodeModel;
 
 namespace T4TemplateWriter.Helpers.ObjectiveC
 {
-	public static class PropertyHelper
-	{
-		public static string Prefix = "";
+    public static class PropertyHelper
+    {
+        public static string Prefix = "";
 
-		public static string GetTypeString(this OdcmType type)
-		{
-			if (type == null)
-			{
-				return "int";
-			}
-			switch (type.Name)
-			{
-			case "String":
-				return "NSString";
-			case "Int32":
-				return "int";
-			case "Int64":
-				return "int";
-			case "Guid":
-				return "NSString";
-			case "DateTimeOffset":
-				return "NSDate";
-			case "Binary":
-				return "NSData";
-			case "Boolean":
-				return "BOOL";
-			case "Stream":
-				return "NSStream";
-			default:
-				return Prefix + type.Name;
-			}
-		}
+        public static string GetTypeString(this OdcmType type)
+        {
+            if (type == null)
+            {
+                return "int";
+            }
+            switch (type.Name)
+            {
+                case "String":
+                    return "NSString";
+                case "Int32":
+                    return "int";
+                case "Int64":
+                    return "int";
+                case "Guid":
+                    return "NSString";
+                case "DateTimeOffset":
+                    return "NSDate";
+                case "Binary":
+                    return "NSData";
+                case "Boolean":
+                    return "BOOL";
+                case "Stream":
+                    return "NSStream";
+                default:
+                    return Prefix + type.Name;
+            }
+        }
 
-		public static string GetTypeString(this OdcmProperty property)
-		{
-			return GetTypeString(property.Type);
-		}
+        public static string GetTypeString(this OdcmProperty property)
+        {
+            return GetTypeString(property.Type);
+        }
 
-		public static bool IsComplex(this OdcmProperty property)
-		{
-			string t = property.GetTypeString();
-			return !(t == "int" || t == "BOOL" || t == "Byte");
-		}
+        public static bool IsComplex(this OdcmProperty property)
+        {
+            string t = property.GetTypeString();
+            return !(t == "int" || t == "BOOL" || t == "Byte");
+        }
 
-		public static bool IsComplex(this OdcmType type)
-		{
-			string t = type.GetTypeString();
-			return !(t == "int" || t == "BOOL" || t == "Byte");
-		}
+        public static bool IsComplex(this OdcmType type)
+        {
+            string t = type.GetTypeString();
+            return !(t == "int" || t == "BOOL" || t == "Byte");
+        }
 
-		public static string ToPropertyString(this OdcmProperty property)
-		{
-			return string.Format("{0} {1}{2}",property.GetFullType(), (property.IsComplex() ? "*" : string.Empty), GetName(property.Name));
-		}
+        public static string ToSetterTypeString(this OdcmProperty property)
+        {
+            return string.Format("{0} {1}", property.GetFullType(), (property.IsComplex() ? "*" : string.Empty));
+        }
 
-		public static string GetName(string name)
-		{
-			if (name.Trim() == "description") return "$$__description";
+        public static string ToPropertyString(this OdcmProperty property)
+        {
+            return string.Format("{0} {1}{2}", property.GetFullType(), (property.IsComplex() ? "*" : string.Empty), GetName(property.Name));
+        }
 
-			if (name.Trim() == "default") return "$$__default";
+        public static string GetName(string name)
+        {
+            if (name.Trim() == "description") return "$$__description";
 
-			return name;
-		}
+            if (name.Trim() == "default") return "$$__default";
 
-		public static string GetFullType(this OdcmProperty property)
-		{
-			string result;
-			if (property.IsCollection)
-				result = !property.IsSystem() && property.GetTypeString() != "NSData" ?
-					string.Format("NSMutableArray<{0}>", property.GetTypeString()) : "NSMutableArray";
-			else
-				result = property.GetTypeString();
+            return name;
+        }
 
-			return result;
-		}
+        public static string GetFullType(this OdcmProperty property)
+        {
+            string result;
+            if (property.IsCollection)
+                result = !property.IsSystem() && property.GetTypeString() != "NSData" ?
+                    string.Format("NSMutableArray<{0}>", property.GetTypeString()) : "NSMutableArray";
+            else
+                result = property.GetTypeString();
 
-		public static string GetFullType(this OdcmType type)
-		{
-			return type.GetTypeString();
-		}
+            return result;
+        }
 
-		public static bool IsSystem(this OdcmProperty property)
-		{
-			string t = property.GetTypeString();
-			return (t == "int" || t == "BOOL" || t == "Byte" || t == "NSString" || t == "NSDate");
-		}
+        public static string GetFullType(this OdcmType type)
+        {
+            return type.GetTypeString();
+        }
 
-		public static bool IsSystem(this OdcmType type)
-		{
-			string t = type.GetTypeString();
-			return (t == "int" || t == "BOOL" || t == "Byte" || t == "NSString" || t == "NSDate");
-		}
+        public static bool IsSystem(this OdcmProperty property)
+        {
+            string t = property.GetTypeString();
+            return (t == "int" || t == "BOOL" || t == "Byte" || t == "NSString" || t == "NSDate");
+        }
 
-		public static string GetToLowerFirstCharName(this OdcmProperty property)
-		{
-			return property.Name.ToLowerFirstChar();
-		}
+        public static bool IsSystem(this OdcmType type)
+        {
+            string t = type.GetTypeString();
+            return (t == "int" || t == "BOOL" || t == "Byte" || t == "NSString" || t == "NSDate");
+        }
 
-		public static string GetToLowerImport(this OdcmProperty property)
-		{
-			var index = property.Type.Name.LastIndexOf('.');
-			return property.Type.Name.Substring(0, index).ToLower() + property.Type.Name.Substring(index);
-		}
+        public static string GetToLowerFirstCharName(this OdcmProperty property)
+        {
+            return property.Name.ToLowerFirstChar();
+        }
 
-		public static string ToLowerFirstChar(this string name)
-		{
-			return Char.ToLowerInvariant(name[0]) + name.Substring(1);
-		}
+        public static string GetToLowerImport(this OdcmProperty property)
+        {
+            var index = property.Type.Name.LastIndexOf('.');
+            return property.Type.Name.Substring(0, index).ToLower() + property.Type.Name.Substring(index);
+        }
+
+        public static string ToLowerFirstChar(this string name)
+        {
+            return Char.ToLowerInvariant(name[0]) + name.Substring(1);
+        }
 
         public static string ToUpperFirstChar(this string name)
         {
             return Char.ToUpperInvariant(name[0]) + name.Substring(1);
         }
 
-		public static bool IsEnum(this OdcmProperty property)
-		{
-			return property.Type is OdcmEnum;
-		}
-	}
+        public static bool IsEnum(this OdcmProperty property)
+        {
+            return property.Type is OdcmEnum;
+        }
+    }
 }
