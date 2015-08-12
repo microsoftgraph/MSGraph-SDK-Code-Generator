67902d58b2e71e467192044548d5dd8af409cfc7
Ace Levenberg <aclev@microsoft.com>
2015-08-12 14:55:17 -0700

    added enums

---
+++ src/T4TemplateWriter/TemplateProcessor/Enums/SubProcessor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Vipr.T4TemplateWriter.TemplateProcessor
+{
+  public enum SubProcessor
+    {
+        EntityType,
+        ComplexType,
+        EnumType,
+        EntityContainer,
+        Other
+    }
+}
