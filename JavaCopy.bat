dbf55fc0f89c7c6b0de6df1d27a45d3abddc0e7e
Peter Nied <peternied@hotmail.com>
2015-10-07 15:38:02 -0700

    Making the copy tool work on all machines

--- JavaCopy.bat
+++ JavaCopy.bat
@@ -1,6 +1,6 @@
 LOCAL
-SET SRCDIR=C:\Users\pnied\Documents\git\vipr-t4templatewriter\src\T4TemplateWriter\bin\debug\output\com\oneDrive
-SET DESTDIR=C:\Users\pnied\Documents\git\onedrive-explorer-android\onedrivesdk\src\main\java\com\onedrive\sdk
+SET SRCDIR="%userprofile%\Documents\git\vipr-t4templatewriter\src\T4TemplateWriter\bin\debug\output\com\oneDrive"
+SET DESTDIR="%userprofile%\Documents\git\onedrive-explorer-android\onedrivesdk\src\main\java\com\onedrive\sdk"
 
 ROBOCOPY %SRCDIR%\generated  %DESTDIR%\generated  /MIR
 ROBOCOPY %SRCDIR%\extensions %DESTDIR%\extensions /MIR
