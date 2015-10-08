5113fa288f1b8424347a95e05efec3a3e3f65813
Peter Nied <peternied@hotmail.com>
2015-10-07 23:55:30 -0700

    Make the copy script universal

--- JavaCopy.bat
+++ JavaCopy.bat
@@ -1,8 +1,8 @@
 LOCAL
-SET SRCDIR="%userprofile%\Documents\git\vipr-t4templatewriter\src\T4TemplateWriter\bin\debug\output\com\oneDrive"
-SET DESTDIR="%userprofile%\Documents\git\onedrive-explorer-android\onedrivesdk\src\main\java\com\onedrive\sdk"
+SET SRCDIR=%userprofile%\Documents\git\vipr-t4templatewriter\src\T4TemplateWriter\bin\debug\output\com\oneDrive
+SET DESTDIR=%userprofile%\Documents\git\onedrive-explorer-android\onedrivesdk\src\main\java\com\onedrive\sdk
 
-ROBOCOPY %SRCDIR%\generated  %DESTDIR%\generated  /MIR
-ROBOCOPY %SRCDIR%\extensions %DESTDIR%\extensions /MIR
+ROBOCOPY "%SRCDIR%\generated"  "%DESTDIR%\generated"  /MIR
+ROBOCOPY "%SRCDIR%\extensions" "%DESTDIR%\extensions" /MIR
 
 ENDLOCAL
