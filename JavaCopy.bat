8b4de922ed199b83d362bbcbb26a8fad6d9367cc
Peter Nied <pnied@microsoft.com>
2015-10-09 17:15:05 -0700

    Don't mirror during copies anymore

--- JavaCopy.bat
+++ JavaCopy.bat
@@ -2,7 +2,7 @@ LOCAL
 SET SRCDIR=%userprofile%\Documents\git\vipr-t4templatewriter\src\T4TemplateWriter\bin\debug\output\com\oneDrive
 SET DESTDIR=%userprofile%\Documents\git\onedrive-explorer-android\onedrivesdk\src\main\java\com\onedrive\sdk
 
-ROBOCOPY "%SRCDIR%\generated"  "%DESTDIR%\generated"  /MIR
-ROBOCOPY "%SRCDIR%\extensions" "%DESTDIR%\extensions" /MIR
+ROBOCOPY "%SRCDIR%\generated"  "%DESTDIR%\generated"
+ROBOCOPY "%SRCDIR%\extensions" "%DESTDIR%\extensions"
 
 ENDLOCAL
