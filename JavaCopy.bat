5302d07d4f75fa6ef5e59572186b8835ec62ebca
Peter Nied <peternied@hotmail.com>
2016-03-19 10:08:14 -0700

    Delete outdated components

--- JavaCopy.bat
+++
@@ -1,8 +0,0 @@
-LOCAL
-SET SRCDIR=%userprofile%\Documents\git\vipr-t4templatewriter\src\T4TemplateWriter\bin\debug\output\com\oneDrive
-SET DESTDIR=%userprofile%\Documents\git\onedrive-explorer-android\onedrivesdk\src\main\java\com\onedrive\sdk
-
-ROBOCOPY "%SRCDIR%\generated"  "%DESTDIR%\generated"
-:: ROBOCOPY "%SRCDIR%\extensions" "%DESTDIR%\extensions"
-
-ENDLOCAL
--- test/T4TemplateWriterTests/Program.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
-
-namespace T4TemplateWriterTests {
-    static class Program {
-        static public void Main(String[] args) {
-            new IntegrationTest().Run(false);
-        }
-    }
-}
