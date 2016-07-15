982d516706f616e9cb90bf4606ddd30d194f70ac
Dan Silver <dansil@microsoft.com>
2016-07-15 09:13:38 -0700

    Initial typescript definition generation

---
+++ src/GraphODataTemplateWriter/CodeHelpers/TypeScript/CodeWriterTypeScript.cs
@@ -0,0 +1,76 @@
+// Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+
+namespace Microsoft.Graph.ODataTemplateWriter.CodeHelpers.TypeScript
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Vipr.Core.CodeModel;
+
+    public class CodeWriterTypeScript : CodeWriterBase
+    {
+        public CodeWriterTypeScript() : base() { }
+
+        public CodeWriterTypeScript(OdcmModel model) : base(model) { }
+
+        public override String WriteOpeningCommentLine()
+        {
+            return "/****************************************************************************" + this.NewLineCharacter;
+        }
+
+        public override String WriteClosingCommentLine()
+        {
+            return "*****************************************************************************/" + this.NewLineCharacter;
+        }
+
+        public override string WriteInlineCommentChar()
+        {
+            return "// ";
+        }
+
+        public override String NewLineCharacter
+        {
+            get { return "\n"; }
+        }
+
+        public IEnumerable<OdcmProperty> EntityProperties(OdcmClass obj)
+        {
+            return obj.Properties.Where(prop => !prop.IsLink).ToList();
+        }
+
+        public IEnumerable<OdcmProperty> EntityNavigationProperties(OdcmClass obj)
+        {
+            return obj.Properties.Where(prop => prop.IsLink).ToList();
+        }
+
+
+        public static string ConvertToJSTypes(string type)
+        {
+            switch (type)
+            {
+                case "Stream":
+                    return "any";
+                case "Int16":
+                case "Int32":
+                case "Int64":
+                case "Double":
+                case "Binary": // let binary: number = 0b1010;
+                    return "Number";
+                case "Guid":
+                    return "String";
+                case "DateTimeOffset":
+                    return "Date";
+            }
+            return type;
+        }
+
+        public String FullTypeName(OdcmProperty prop)
+        {
+            var Name = prop.Projection.Type.Name;
+            Name = ConvertToJSTypes(Name);
+
+            return (prop.IsCollection) ? "[" + Name + "]" : Name;
+        }
+    }
+
+}
