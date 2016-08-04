9a20d7e0e0fea939d753d81dbf9d496e289201ab
Kevin Lam <kevinlam@microsoft.com>
2016-08-04 16:46:19 -0700

    Add codegen commit info to generated files

--- src/GraphODataTemplateWriter/CodeHelpers/CodeWriterBase.cs
+++ src/GraphODataTemplateWriter/CodeHelpers/CodeWriterBase.cs
@@ -4,13 +4,18 @@ namespace Microsoft.Graph.ODataTemplateWriter.CodeHelpers
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
     using System.Text;
+    using System.Windows.Forms;
+    using LibGit2Sharp;
     using Microsoft.Graph.ODataTemplateWriter.Settings;
     using Vipr.Core.CodeModel;
 
     abstract public class CodeWriterBase
     {
+        private static String CommitHash { get; set; }
+
         public OdcmModel CurrentModel
         {
             get;
@@ -61,9 +66,37 @@ namespace Microsoft.Graph.ODataTemplateWriter.CodeHelpers
                             .Select(line => this.WriteInlineCommentChar() + line)
                             .ToArray()) + this.NewLineCharacter
                     : "",
+                WriteInlineCommentChar() + this.NewLineCharacter,
+                WriteInlineCommentChar() + "CodeGen: " + GetGitCommit() + this.NewLineCharacter,
                 this.WriteClosingCommentLine()
               });
+        }
+        
+        public static String GetGitCommit()
+        {
+            if (CodeWriterBase.CommitHash == null)
+            {
+                using (var repo = new Repository(Repository.Discover(Directory.GetCurrentDirectory())))
+                {
+                    var diff =
+                        repo.Diff.Compare<Patch>(repo.Head.Tip.Tree, DiffTargets.WorkingDirectory | DiffTargets.Index);
+                    bool dirty = !String.IsNullOrEmpty(diff.Content);
 
+                    Commit tip = repo.Head.Tip;
+                    CodeWriterBase.CommitHash = tip.Sha + (dirty ? "*" : "");
+
+                    if (dirty)
+                    {
+                        MessageBox.Show(
+                            "Your git status is dirty, please commit and re-generate before publishing to SDK's! This will help ensure that we can identify the exact code that generated a given iteration of the SDK.",
+                            "WARNING",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Warning);
+                    }
+                }
+            }
+
+            return CodeWriterBase.CommitHash;
         }
     }
 }
