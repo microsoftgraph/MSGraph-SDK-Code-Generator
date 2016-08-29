e8e208974308ca5eddec2f3e853e7e08c3c91711
Caitlin Bales (MSFT) <c.bales@outlook.com>
2016-08-29 16:05:05 -0700

    Adds PHP model generation

---
+++ src/GraphODataTemplateWriter/PathWriters/PHPPathWriter.cs
@@ -0,0 +1,55 @@
+// Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+
+namespace Microsoft.Graph.ODataTemplateWriter.PathWriters
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using Microsoft.Graph.ODataTemplateWriter.Extensions;
+    using Microsoft.Graph.ODataTemplateWriter.Settings;
+    using Microsoft.Graph.ODataTemplateWriter.TemplateProcessor;
+
+    public class PHPPathWriter : PathWriterBase
+    {
+
+        public override String WritePath(ITemplateInfo template, String entityTypeName)
+        {
+            var theNamespace = this.CreateNamespace(template.TemplateType.ToString().ToUpperFirstChar());
+            var namespacePath = this.CreatePathFromNamespace(theNamespace);
+
+            var fileName = Extensions.StringExtensions.ToCheckedCase(this.TransformFileName(template, entityTypeName));
+
+            String filePath = Path.Combine(namespacePath, fileName);
+            return filePath;
+        }
+
+        private string CreateNamespace(string folderName)
+        {
+            var @namespace = this.Model.GetNamespace();
+            var prefix = ConfigurationService.Settings.NamespacePrefix;
+
+            if (string.IsNullOrEmpty(folderName))
+            {
+                return string.IsNullOrEmpty(prefix) ? @namespace
+                                                    : string.Format("{0}.{1}", prefix, @namespace);
+            }
+            return string.IsNullOrEmpty(prefix) ? string.Format("{0}.{1}", @namespace, folderName)
+                                                : string.Format("{0}.{1}.{2}", prefix, @namespace, folderName);
+        }
+
+        private string CreatePathFromNamespace(string @namespace)
+        {
+            var splittedPaths = @namespace.Split('.');
+
+            //Capitalize all but the src directory
+            for (int i = 1; i < splittedPaths.Length; i++)
+            {
+                splittedPaths[i] = splittedPaths[i].ToUpperFirstChar();
+            }
+
+            var destinationPath = splittedPaths.Aggregate(string.Empty, (current, path) =>
+                                  current + string.Format("{0}{1}", path, Path.DirectorySeparatorChar));
+            return destinationPath;
+        }
+    }
+}
