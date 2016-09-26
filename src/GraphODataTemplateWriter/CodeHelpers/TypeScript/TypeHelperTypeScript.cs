b02837aa954a1469830653cb4955a437ba27ed45
Dan Silver <dansil@microsoft.com>
2016-09-26 10:32:36 -0700

    Updated TypeScript template

--- src/GraphODataTemplateWriter/CodeHelpers/TypeScript/TypeHelperTypeScript.cs
+++ src/GraphODataTemplateWriter/CodeHelpers/TypeScript/TypeHelperTypeScript.cs
@@ -35,6 +35,7 @@ namespace Microsoft.Graph.ODataTemplateWriter.CodeHelpers.TypeScript
             switch (typeStr)
             {
                 case "Stream":
+                case "Json":
                     typeStr = "any";
                     break;
                 case "Int16":
@@ -65,57 +66,7 @@ namespace Microsoft.Graph.ODataTemplateWriter.CodeHelpers.TypeScript
         {
             return char.ToUpper(s[0]) + s.Substring(1);
         }
-
-        public static OdcmClass FindEntityByName(this IEnumerable<OdcmClass> entities, String name)
-        {
-            foreach (var entity in entities)
-            {
-                if (entity.Name.Equals(name))
-                {
-                    return entity;
-                }
-            }
-            return null;
-        }
-
-        public static List<string> GetNavigationPaths(this OdcmProperty prop, string baseString, IEnumerable<OdcmClass> entities, int depth)
-        {
-            List<String> paths = new List<string>();
-            if (depth > 5) return paths;
-            OdcmClass entity = entities.FindEntityByName(prop.Type.Name);
-
-            string newBase;
-            if (prop.IsCollection)
-            {
-                newBase = baseString + "/" + prop.Name + "/{" + prop.Type.Name + "Id}";
-                paths.Add(baseString + "/" + prop.Name);
-            }
-            else
-            {
-                newBase = baseString + "/" + prop.Name;
-         
-            }
-
-            paths.Add(newBase);
-            if (entity.Methods.Any()) // check for actions
-            {
-                foreach (var method in entity.Methods)
-                {
-                    paths.Add(newBase + "/" + method.Name);
-                }
-            }
-
-            foreach (var eprop in entity.Properties)
-            {
-                if (eprop.IsLink)
-                {
-                    paths.AddRange(GetNavigationPaths(eprop, newBase, entities, depth + 1));
-                }
-            }
-
-
-            return paths;
-        }
+        
         
     }
 }
