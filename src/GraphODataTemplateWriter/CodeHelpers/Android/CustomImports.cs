99ab691a9e9f3e06d39748cb7d6afbf96259192c
Caitlin Bales (MSFT) <c.bales@outlook.com>
2017-07-27 14:04:30 -0700

    Add custom import statements
    Fixes manual addition of custom import statement for:
    - UploadSession

---
+++ src/GraphODataTemplateWriter/CodeHelpers/Android/CustomImports.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Graph.ODataTemplateWriter.CodeHelpers.Android
+{
+    public class CustomImports
+    {
+        public const string UploadSession = "import com.microsoft.graph.concurrency.ChunkedUploadProvider;\r\n";
+    }
+}
