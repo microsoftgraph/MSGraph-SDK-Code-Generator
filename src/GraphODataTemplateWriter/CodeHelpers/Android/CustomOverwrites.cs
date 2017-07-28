96a95b4326004290eead2d8bbd90a197bcaf9b38
Caitlin Bales (MSFT) <c.bales@outlook.com>
2017-07-28 16:26:32 -0700

    Add ability to overwrite existing methods
    Fixes manual editing of methods:
    - BaseOnenotePageCollectionRequest
    - IBaseOnenotePageCollectionRequest
    - BaseOnenotePageRequest
    - IBaseOnenotePageRequest

---
+++ src/GraphODataTemplateWriter/CodeHelpers/Android/CustomOverwrites.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Graph.ODataTemplateWriter.CodeHelpers.Android
+{
+    /**
+     * This class contains a list of constants that define code snippets that should
+     * be overwritten at generation time 
+     * The field name should be equal to the class name you wish to edit
+     * The dictionary value should contain two string, the first is the search value
+     * and the latter is the replace value
+     * This will be used in BaseModel.template.tt in the PostProcess() method
+     */
+    public class CustomOverwrites
+    {
+        public static Dictionary<string, string> BaseOnenotePageCollectionRequest = new Dictionary<string, string>() {
+            {
+                "public void post(final OnenotePage newOnenotePage, final ICallback<OnenotePage> callback) {",
+                "public void post(final byte[] newOnenotePage, final ICallback<OnenotePage> callback) {"
+            },
+            {
+                "public OnenotePage post(final OnenotePage newOnenotePage) throws ClientException {",
+                "public OnenotePage post(final byte[] newOnenotePage) throws ClientException {"
+            }
+        };
+
+        public static Dictionary<string, string> BaseOnenotePageRequest = new Dictionary<string, string>()
+        {
+            {
+                "public void post(final OnenotePage newOnenotePage, final ICallback<OnenotePage> callback) {",
+                "public void post(final byte[] newOnenotePage, final ICallback<OnenotePage> callback) {"
+            },
+            {
+                "public OnenotePage post(final OnenotePage newOnenotePage) throws ClientException {",
+                "public OnenotePage post(final byte[] newOnenotePage) throws ClientException {"
+            }
+        };
+
+        public static Dictionary<string, string> IBaseOnenotePageCollectionRequest = new Dictionary<string, string>()
+        {
+            {
+                "void post(final OnenotePage newOnenotePage, final ICallback<OnenotePage> callback);",
+                "void post(final byte[] newOnenotePage, final ICallback<OnenotePage> callback);"
+            },
+            {
+                "OnenotePage post(final OnenotePage newOnenotePage) throws ClientException;",
+                "OnenotePage post(final byte[] newOnenotePage) throws ClientException;"
+            }
+        };
+
+        public static Dictionary<string, string> IBaseOnenotePageRequest = new Dictionary<string, string>()
+        {
+            {
+                "void post(final OnenotePage newOnenotePage, final ICallback<OnenotePage> callback);",
+                "void post(final byte[] newOnenotePage, final ICallback<OnenotePage> callback);"
+            },
+            {
+                "OnenotePage post(final OnenotePage newOnenotePage) throws ClientException;",
+                "OnenotePage post(final byte[] newOnenotePage) throws ClientException;"
+            }
+        };
+    }
+}
