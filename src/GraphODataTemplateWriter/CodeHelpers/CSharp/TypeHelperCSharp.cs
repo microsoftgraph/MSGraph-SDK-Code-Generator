2fa015fc750663083b0312459d8775f3d1d093d4
Kamil Sikorski <kasikors@microsoft.com>
2019-08-07 13:15:54 -0400

    Fixed c# xml docs.

--- src/GraphODataTemplateWriter/CodeHelpers/CSharp/TypeHelperCSharp.cs
+++ src/GraphODataTemplateWriter/CodeHelpers/CSharp/TypeHelperCSharp.cs
@@ -223,11 +223,9 @@ namespace Microsoft.Graph.ODataTemplateWriter.CodeHelpers.CSharp
 
             if (description != null)
             {
-                return description.Replace("<", "&lt;")
-                                  .Replace(">", "&gt;")
-                                  .Replace("&", "&amp;")
-                                  .Replace("\r\n", "\r\n///"); // &#xD;&#xA; The HTML encoded has already been converted to escaped chars.
+                return description.Replace("\r\n", "\r\n///"); // &#xD;&#xA; The HTML encoded has already been converted to escaped chars.
             }
+
             return null;
         }
 
