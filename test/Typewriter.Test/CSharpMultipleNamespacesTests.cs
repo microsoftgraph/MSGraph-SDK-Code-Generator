42e5a1b14661c5d4d55aae8d2e9bfbcb2ae42aa2
Mustafa Zengin <muzengin@microsoft.com>
2020-07-23 16:15:42 -0700

    refactor multiple namespaces to support multiple languages, add Java test data

---
+++ test/Typewriter.Test/CSharpMultipleNamespacesTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Typewriter.Test
+{
+    [TestClass]
+    public class CSharpMultipleNamespacesTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            MultipleNamespacesTestRunner.Run(TestLanguage.CSharp);
+        }
+    }
+}
---
+++ test/Typewriter.Test/TestDataJava/com/microsoft/graph2/callrecords/models/generated/CallType.java
@@ -0,0 +1,25 @@
+// ------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------
+
+package com.microsoft.graph2.callrecords.models.generated;
+
+
+/**
+ * The Enum Call Type.
+*/
+public enum CallType
+{
+    /**
+    * unknown
+    */
+    UNKNOWN,
+    /**
+    * group Call
+    */
+    GROUP_CALL,
+    /**
+    * For CallType values that were not expected from the service
+    */
+    UNEXPECTED_VALUE
+}
