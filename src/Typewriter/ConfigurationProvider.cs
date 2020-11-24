589588a6537f996a94861f1c981726f4605899b8
Vincent Biret <vibiret@microsoft.com>
2020-11-24 15:15:18 -0500

    - adds failover for dotnet run context

--- src/Typewriter/ConfigurationProvider.cs
+++ src/Typewriter/ConfigurationProvider.cs
@@ -9,14 +9,15 @@ namespace Typewriter
     public class ConfigurationProvider : Vipr.Core.IConfigurationProvider
     {
         private readonly IConfiguration Configuration;
+        private const string dotNetCorePathprefix = "../GraphODataTemplateWriter";
         private const string configFolder = ".config";
         public ConfigurationProvider()
         {
             var builder = new ConfigurationBuilder();
             if (!string.IsNullOrWhiteSpace(Environment.CurrentDirectory))
             {
-                builder.AddJsonFile(Path.Combine(Environment.CurrentDirectory, configFolder, "TemplateWriterSettings.json"), true);
-                builder.AddJsonFile(Path.Combine(Environment.CurrentDirectory, configFolder, "GraphEndpointListSettings.json"), true);
+                builder.AddJsonFile(GetConfigurationFilePath("TemplateWriterSettings.json"), true);
+                builder.AddJsonFile(GetConfigurationFilePath("GraphEndpointListSettings.json"), true);
             }
 
             Configuration = builder.AddEnvironmentVariables().Build();
@@ -26,11 +27,19 @@ namespace Typewriter
             var builder = new ConfigurationBuilder();
             if (!string.IsNullOrWhiteSpace(Environment.CurrentDirectory))
             {
-                builder.AddJsonFile(Path.Combine(Environment.CurrentDirectory, configFolder, "TemplateWriterSettings.json"), true);
-                builder.AddJsonFile(Path.Combine(Environment.CurrentDirectory, configFolder, $"{targetLanguage}Settings.json"), false);
+                builder.AddJsonFile(GetConfigurationFilePath("TemplateWriterSettings.json"), true);
+                builder.AddJsonFile(GetConfigurationFilePath($"{targetLanguage}Settings.json"), false);
             }
 
             Configuration = builder.AddEnvironmentVariables().Build();
+        }
+        private string GetConfigurationFilePath(string fileName)
+        { //when using dotnet run the Environment.CurrentDirectory is set to csproj directory, not the bin like when runing with Visual Studio
+            var regularPath = Path.Combine(Environment.CurrentDirectory, configFolder, fileName);
+            if (File.Exists(regularPath))
+                return regularPath;
+            else
+                return Path.Combine(Environment.CurrentDirectory, dotNetCorePathprefix, configFolder, fileName);
         }
 
         public T GetConfiguration<T>()
