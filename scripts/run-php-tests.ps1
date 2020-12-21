530621e0fdf77c01dd64cb8a43cf3115cd663284
Mustafa Zengin <muzengin@microsoft.com>
2020-12-21 14:07:07 -0800

    specify php version

--- scripts/run-php-tests.ps1
+++
@@ -1,5 +0,0 @@
-# https://docs.microsoft.com/en-us/azure/devops/pipelines/ecosystems/php?view=azure-devops
-
-php -v
-composer install --no-interaction --prefer-dist
-./vendor/bin/phpunit --exclude-group functional
