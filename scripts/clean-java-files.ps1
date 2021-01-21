d43ff598a4f801876d52952d8d901a768cbefa8f
Vincent Biret <vibiret@microsoft.com>
2021-01-21 14:51:08 -0500

    - removes excluded hand files for generation cleanup script

--- scripts/clean-java-files.ps1
+++ scripts/clean-java-files.ps1
@@ -1,9 +1,7 @@
 $modelsAndRequestsDirectories = Get-ChildItem $env:MainDirectory -Include models,requests -Recurse -Directory
 
-# this list should be updated if a new hand-crafted extension is added to one of the directories
-$filesThatShouldNotBeDeleted = @()
 foreach ($directory in $modelsAndRequestsDirectories)
 {
-    Remove-Item $directory.FullName -Recurse -Force -Exclude $filesThatShouldNotBeDeleted
+    Remove-Item $directory.FullName -Recurse -Force
 }
 Write-Host "Removed the existing generated files in the repo's main directory: $env:MainDirectory" -ForegroundColor Green
