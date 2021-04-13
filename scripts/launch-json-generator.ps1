20feaf82d98a4c6a026a64bf10aae6795c67104c
Mustafa Zengin <muzengin@microsoft.com>
2021-04-13 14:34:12 -0700

    output folder rename, test commands

--- scripts/launch-json-generator.ps1
+++ scripts/launch-json-generator.ps1
@@ -1,3 +1,68 @@
+function createCommandLineArgs
+{
+    param (
+        [string]$metadata,
+        [string]$endpoint,
+        [string]$language,
+        [string]$outputFolder
+    )
+
+    $commandLineArgs = @(
+        "-v",
+        "Info",
+        "-m",
+        $metadata,
+        "-g",
+        "Files",
+        "-l",
+        $language,
+        "-o",
+        $outputFolder,
+        "-e",
+        $endpoint
+    )
+
+    if ($endpoint -eq "beta")
+    {
+        if ($language -eq "PHP")
+        {
+            $commandLineArgs += @(
+                "-p",
+                "php.namespacePrefix:Beta"
+            )
+        }
+        elseif ($language -eq "TypeScript")
+        {
+            $commandLineArgs += @(
+                "-p",
+                "typescript.namespacePostfix:beta"
+            )
+        }
+    }
+
+    return $commandLineArgs
+}
+
+function createConfiguration
+{
+    param (
+        [string]$name,
+        [string[]]$commandLineArgs
+    )
+
+    [ordered]@{
+        name = $name
+        type = "coreclr"
+        request = "launch"
+        preLaunchTask = "build"
+        program = "`${workspaceFolder}/src/Typewriter/bin/Debug/net5.0/Typewriter.dll"
+        args = $commandLineArgs
+        cwd = "`${workspaceFolder}/src/Typewriter/bin/Debug/net5.0"
+        console = "internalConsole"
+        stopAtEntry = $false
+    }
+}
+
 $obj = [ordered]@{
     version = "0.2.0"
     configurations = @()
@@ -5,61 +70,45 @@ $obj = [ordered]@{
 
 $languages = "CSharp","PHP","TypeScript","Java","ObjC"
 $endpoints = "beta","v1.0"
-$metadata = @{
+$prodMetadata = @{
     "beta" = "`${workspaceFolder}/../msgraph-metadata/clean_beta_metadata/cleanMetadataWithDescriptionsbeta.xml";
     "v1.0" = "`${workspaceFolder}/../msgraph-metadata/clean_v10_metadata/cleanMetadataWithDescriptionsv1.0.xml"
+}
+
+$metadataMultipleNamespacesFile = "`${workspaceFolder}/test/Typewriter.Test/Metadata/MetadataMultipleNamespaces.xml"
+$metadataWithSubNamespacesFile = "`${workspaceFolder}/test/Typewriter.Test/Metadata/MetadataWithSubNamespaces.xml"
+
+$testMetadata = @{
+    CSharp = $metadataMultipleNamespacesFile
+    Java = $metadataMultipleNamespacesFile
+    TypeScript = $metadataWithSubNamespacesFile
+    PHP = $metadataWithSubNamespacesFile
+    ObjC = $metadataWithSubNamespacesFile
+}
+
+$testData = @{
+    "beta" = "Java","PHP","TypeScript"
+    "v1.0" = "CSharp","Java","ObjC","PHP","TypeScript"
 }
 
 foreach($language in $languages)
 {
     foreach($endpoint in $endpoints)
     {
-        $commandLineArgs = @(
-            "-v",
-            "Info",
-            "-m",
-            $metadata[$endpoint],
-            "-g",
-            "Files",
-            "-l",
-            $language,
-            "-o",
-            "`${workspaceFolder}/../Output$language$endpoint",
-            "-e",
-            $endpoint
-        )
+        $commandLineArgs = createCommandLineArgs $prodMetadata[$endpoint] $endpoint $language "`${workspaceFolder}/../generator-output-$language-$endpoint"
+        $obj.configurations += createConfiguration "Generate $language $endpoint" $commandLineArgs
 
-        if ($endpoint -eq "beta")
+        if ($testData[$endpoint] -contains $language)
         {
-            if ($language -eq "PHP")
+            $postFix = ""
+            if ($endpoint -eq "beta")
             {
-                $commandLineArgs += @(
-                    "-p",
-                    "php.namespacePrefix:Beta"
-                )
+                $postFix = "Beta"
             }
-            elseif ($language -eq "TypeScript")
-            {
-                $commandLineArgs += @(
-                    "-p",
-                    "typescript.namespacePostfix:beta"
-                )
-            }
-        }
 
-        $configuration = [ordered]@{
-            name = "Generate $language $endpoint"
-            type = "coreclr"
-            request = "launch"
-            preLaunchTask = "build"
-            program = "`${workspaceFolder}/src/Typewriter/bin/Debug/net5.0/Typewriter.dll"
-            args = $commandLineArgs
-            cwd = "`${workspaceFolder}/src/Typewriter/bin/Debug/net5.0"
-            console = "internalConsole"
-            stopAtEntry = $false
+            $testDataCommandLineArgs = createCommandLineArgs $testMetadata[$language] $endpoint $language "`${workspaceFolder}/test/TestData$language$postFix"
+            $obj.configurations += createConfiguration "Update Test Data $language $endpoint" $testDataCommandLineArgs
         }
-
-        $obj.configurations += $configuration
     }
 }
 
