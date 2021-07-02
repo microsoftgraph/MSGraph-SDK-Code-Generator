688e11b41b73c5202bf1f08adebea9ea043b7fda
silaskenneth <silaskenneth1@gmail.com>
2021-07-02 23:20:40 +0300

    Fix deletion of the GraphConstants.php file

--- scripts/copy-php-beta-migration-models.ps1
+++ scripts/copy-php-beta-migration-models.ps1
@@ -5,7 +5,6 @@ Get-ChildItem $env:RepoModelsDir -Exclude *.php | Remove-Item -Force -Recurse
 Write-Host "Removed the existing generated files for Beta in the repo at $($env:RepoModelsDir)." -ForegroundColor Green
 
 $betaGeneratedFiles = Join-Path $env:OutputFullPath "/com/Beta/Microsoft/Graph/*"
-New-Item -Path $env:RepoModelsDir -ItemType Directory
 
 # copy generated files by preserving the subdirectory structure from /com/Beta
 # the same directory structure is expected to appear in repository's src/Beta directory after copy
