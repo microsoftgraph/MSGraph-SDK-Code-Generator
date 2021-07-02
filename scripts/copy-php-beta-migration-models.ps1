e9c94d26ba1ecd5db651db9761d28610eb46e6ed
silaskenneth <silaskenneth1@gmail.com>
2021-07-02 22:49:54 +0300

    Fix generation path issues

---
+++ scripts/copy-php-beta-migration-models.ps1
@@ -0,0 +1,11 @@
+# removes all models under src/Beta
+Remove-Item $env:RepoModelsDir -Recurse -Force
+Write-Host "Removed the existing generated files for Beta in the repo at $($env:RepoModelsDir)." -ForegroundColor Green
+
+$betaGeneratedFiles = Join-Path $env:OutputFullPath "/com/Beta/Microsoft/Graph/*"
+New-Item -Path $env:RepoModelsDir -ItemType Directory
+
+# copy generated files by preserving the subdirectory structure from /com/Beta
+# the same directory structure is expected to appear in repository's src/Beta directory after copy
+Copy-Item $betaGeneratedFiles $env:RepoModelsDir -Recurse -Force
+Write-Host "Moved the models with path $betaGeneratedFiles into the local repo." -ForegroundColor Green
