e848d35c20fd4c5ae77e4c6de9e2c5612a5e12d5
Andrew Omondi <andrew.omondi@microsoft.com>
2021-07-22 11:31:31 +0300

    Include using system in enum types

--- test/Typewriter.Test/TestDataCSharp/com/microsoft/graph/model/Enum1.cs
+++ test/Typewriter.Test/TestDataCSharp/com/microsoft/graph/model/Enum1.cs
@@ -1,4 +1,4 @@
-﻿// ------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 //  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
 // ------------------------------------------------------------------------------
 
@@ -10,6 +10,7 @@
 
 namespace Microsoft.Graph
 {
+    using System;
     using System.Text.Json.Serialization;
 
     /// <summary>
