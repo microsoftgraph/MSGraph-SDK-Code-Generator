693c4b3c95406094378a2a1909a7053ef9ece1bf
rkodev <43806892+rkodev@users.noreply.github.com>
2022-03-23 19:26:19 +0300

    Fixes download directory

--- scripts/clean-typescript-files.ps1
+++ scripts/clean-typescript-files.ps1
@@ -1,6 +1,5 @@
-$directories = Get-ChildItem -Path $env:MainDirectory -Directory -Filter *src*
-foreach ($directory in $directories) {
-	Remove-Item -Path $directory.FullName -Recurse -Force -Verbose
-}
+
+Rename-Item $env:MainDirectory "lib"
+New-Item $env:MainDirectory -ItemType Directory
 
 Write-Host "Removed the existing generated files in the repo's main directory: $env:MainDirectory" -ForegroundColor Green
