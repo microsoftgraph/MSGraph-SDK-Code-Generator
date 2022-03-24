ae78a236f624e532070f9918ec7f7908dede9b5f
rkodev <43806892+rkodev@users.noreply.github.com>
2022-03-24 11:45:42 +0300

    Delete folder recursive

--- scripts/clean-typescript-files.ps1
+++ scripts/clean-typescript-files.ps1
@@ -1,5 +1,23 @@
+function Delete-Folder-Recursive {
+    param (
+        $FolderName
+    )
 
-Rename-Item $env:MainDirectory "lib"
-New-Item $env:MainDirectory -ItemType Directory
+    $directories = Get-ChildItem -Path $FolderName -Directory
+    foreach ($directory in $directories) {
+    	Delete-Folder-Recursive $directory.FullName
+        Remove-Item -Path $directory.FullName -Recurse -Force -Verbose
+    }
+
+    Write-Host "Deleting folder: $FolderName"
+}
+
+Delete-Folder-Recursive $env:MainDirectory
+
+$files = Get-ChildItem -Path $env:MainDirectory -File  -Filter "*.ts"
+foreach ($file in $files) {
+  Remove-Item -Path $file.FullName -Force
+}
+
 
 Write-Host "Removed the existing generated files in the repo's main directory: $env:MainDirectory" -ForegroundColor Green
