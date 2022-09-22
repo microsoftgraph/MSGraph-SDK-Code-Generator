f8e1fa423c1834622b6a006b03afb1f8395cd1ed
rkodev <43806892+rkodev@users.noreply.github.com>
2022-09-22 21:40:15 +0300

    Remove all files apart from service client and tests

--- scripts/clean-go-files.ps1
+++ scripts/clean-go-files.ps1
@@ -1,7 +1,7 @@
-$directories = Get-ChildItem -Path $env:MainDirectory -Directory -Exclude @("core", "scripts", ".github")
+$directories = Get-ChildItem -Path $env:MainDirectory -Directory -Exclude @("core", "scripts", ".github" , "tests")
 foreach ($directory in $directories) {
 	Remove-Item -Path $directory.FullName -Recurse -Force -Verbose
 }
-Get-ChildItem -Path $env:MainDirectory -Filter graph_base_service_client.go -Recurse | Remove-Item
+Remove-Item -Path $env:MainDirectory -Filter "*.go" -Exclude "graph_request_adapter.go", "graph_service_client.go" -Verbose
 
 Write-Host "Removed the existing generated files in the repo's main directory: $env:MainDirectory" -ForegroundColor Green
