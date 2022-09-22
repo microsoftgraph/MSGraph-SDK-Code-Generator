f73041c7809c52fe2f43f51d64de536cc342a12d
Ronald K <43806892+rkodev@users.noreply.github.com>
2022-09-22 22:51:22 +0300

    Adds a base client for go generation (#835)
    * Adds a base client for go generation
    
    * Remove all files apart from service client and tests

--- scripts/clean-go-files.ps1
+++ scripts/clean-go-files.ps1
@@ -1,7 +1,7 @@
-$directories = Get-ChildItem -Path $env:MainDirectory -Directory -Exclude @("core", "scripts", ".github")
+$directories = Get-ChildItem -Path $env:MainDirectory -Directory -Exclude @("core", "scripts", ".github" , "tests")
 foreach ($directory in $directories) {
 	Remove-Item -Path $directory.FullName -Recurse -Force -Verbose
 }
-Remove-Item -Path $env:MainDirectory -Filter "*.go" -Exclude "graph_request_adapter.go" -Verbose
+Remove-Item -Path $env:MainDirectory -Filter "*.go" -Exclude "graph_request_adapter.go", "graph_service_client.go" -Verbose
 
 Write-Host "Removed the existing generated files in the repo's main directory: $env:MainDirectory" -ForegroundColor Green
