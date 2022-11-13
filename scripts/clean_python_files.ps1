30a15dc98ad05aae6757f233302c3fee01faaa51
samwelkanda <samwelkanda@gmail.com>
2022-11-14 01:02:02 +0300

    Update cleaning script

--- scripts/clean_python_files.ps1
+++ scripts/clean_python_files.ps1
@@ -1,12 +1,3 @@
-$directories = Get-ChildItem -Path $env:MainDirectory -Directory
-foreach ($directory in $directories) {
-	Remove-Item -Path $directory.FullName -Recurse -Force -Verbose
-}
-
-$files = Get-ChildItem -Path $env:MainDirectory -File  -Filter "*.py"
-foreach ($file in $files) {
-  Remove-Item -Path $file.FullName -Force
-}
-
-
-Write-Host "Removed the existing generated files in the repo's main directory: $env:MainDirectory" -ForegroundColor Green
+# Remove old generated files.
+Remove-Item -Recurse $env:RepoModelsDir | Write-Host
+Write-Host "Removed the existing generated files in the directory: $env:RepoModelsDir." -ForegroundColor Green
