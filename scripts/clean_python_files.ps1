a0422e8a219a585343b96e59db929366ae0cbdb1
samwelkanda <samwelkanda@gmail.com>
2022-11-14 01:30:30 +0300

    Remove old file

--- scripts/clean_python_files.ps1
+++
@@ -1,3 +0,0 @@
-# Remove old generated files.
-Remove-Item -Recurse $env:RepoModelsDir | Write-Host
-Write-Host "Removed the existing generated files in the directory: $env:RepoModelsDir." -ForegroundColor Green
