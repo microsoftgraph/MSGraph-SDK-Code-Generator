4506b4b7eef26dffac642200040fd051c93c3c71
Vincent Biret <vibiret@microsoft.com>
2022-12-28 11:50:46 -0500

    - fixes version file name for ruby
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- scripts/clean-ruby-files.ps1
+++ scripts/clean-ruby-files.ps1
@@ -2,6 +2,6 @@ $directories = Get-ChildItem -Path $env:MainDirectory -Directory
 foreach ($directory in $directories) {
 	Remove-Item -Path $directory.FullName -Recurse -Force -Verbose
 }
-Remove-Item -Path $env:MainDirectory -Filter "*.rb" -Exclude "graph_request_adapter.rb", "graph_service_client.rb", "version.rb" -Verbose
+Remove-Item -Path $env:MainDirectory -Filter "*.rb" -Exclude "graph_request_adapter.rb", "graph_service_client.rb", "version_information.rb" -Verbose
 
 Write-Host "Removed the existing generated files in the repo's main directory: $env:MainDirectory" -ForegroundColor Green
