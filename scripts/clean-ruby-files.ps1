b91e126f7d3133f673f7c14cf4247d4f112fe498
Vincent Biret <vibiret@microsoft.com>
2023-02-02 15:44:23 -0500

    - aligns ruby client name with other languages

--- scripts/clean-ruby-files.ps1
+++ scripts/clean-ruby-files.ps1
@@ -3,6 +3,6 @@ foreach ($directory in $directories) {
 	Remove-Item -Path $directory.FullName -Recurse -Force -Verbose
 }
 Get-ChildItem -Path $env:MainDirectory -Directory | ForEach-Object { Remove-Item -Path $_.FullName -Verbose -Force -Recurse}
-Get-ChildItem -Path $env:MainDirectory -Exclude "microsoft_graph_request_adapter.rb", "microsoft_graph_service_client.rb", "version_information.rb" | Where-Object { !$_.PSIsContainer -and $_.Name -like "*.rb" } | ForEach-Object { Remove-Item -Path $_.FullName -Verbose -Force}
+Get-ChildItem -Path $env:MainDirectory -Exclude "graph_request_adapter.rb", "graph_service_client.rb", "version_information.rb" | Where-Object { !$_.PSIsContainer -and $_.Name -like "*.rb" } | ForEach-Object { Remove-Item -Path $_.FullName -Verbose -Force}
 
 Write-Host "Removed the existing generated files in the repo's main directory: $env:MainDirectory" -ForegroundColor Green
