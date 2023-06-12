8e1cb280874385bb3badfe1080b33cf8d1b18733
Ronald K <43806892+rkodev@users.noreply.github.com>
2023-06-12 15:18:00 +0300

    Filter change_notification models to be retained in go generation (#1028)

--- scripts/clean-go-files.ps1
+++ scripts/clean-go-files.ps1
@@ -1,6 +1,6 @@
 $directories = Get-ChildItem -Path $env:MainDirectory -Directory -Exclude @("core", "scripts", ".github" , "tests")
 foreach ($directory in $directories) {
-	Remove-Item -Path $directory.FullName -Recurse -Force -Verbose
+	Remove-Item -Path $directory.FullName -Recurse -Force -Verbose -Exclude *change_notification*, "change_type.go", "lifecycle_event_type.go", "resource_data.go", "resource_permission.go" , "resource_permission_collection_response.go"
 }
 Remove-Item -Path $env:MainDirectory -Filter "*.go" -Exclude "graph_request_adapter.go", "graph_service_client.go" -Verbose
 
