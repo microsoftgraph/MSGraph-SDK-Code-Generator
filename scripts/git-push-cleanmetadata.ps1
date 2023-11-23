e4b47c9e321ab6d10b6c5328903a114a549a24ae
Irvine Sunday <irochand@microsoft.com>
2023-11-23 15:50:56 +0300

    Update branch to push files to

--- scripts/git-push-cleanmetadata.ps1
+++ scripts/git-push-cleanmetadata.ps1
@@ -6,9 +6,9 @@ if ($env:PublishChanges -eq $False)
 
 Write-Host "About to add clean $env:EndpointVersion metadata file....."
 
-git fetch origin master
-# checkout master to move from detached HEAD mode
-git switch master
+git fetch origin is/metadata-pipeline-test
+# checkout is/metadata-pipeline-test to move from detached HEAD mode
+git switch is/metadata-pipeline-test
 
 git add . | Write-Host
 if ($env:BUILD_REASON -eq 'Manual') # Skip CI if manually running this pipeline.
@@ -25,7 +25,7 @@ Write-Host "Added and commited cleaned $env:EndpointVersion metadata." -Foregrou
 # sync branch before pushing
 # this is especially important while running v1 and beta in parallel
 # and one process goes out of sync because of the other's check-in
-git pull origin master --rebase
+git pull origin is/metadata-pipeline-test --rebase
 
-git push --set-upstream origin master | Write-Host
-Write-Host "Pushed the results of the build $env:BUILD_BUILDID to the master branch." -ForegroundColor Green
+git push --set-upstream origin is/metadata-pipeline-test | Write-Host
+Write-Host "Pushed the results of the build $env:BUILD_BUILDID to the is/metadata-pipeline-test branch." -ForegroundColor Green
