07ff1f98e17297a8eefa53d2992034c99bc52395
Vincent Biret <vibiret@microsoft.com>
2024-01-15 16:21:19 -0500

    - makes package name configurable for the script
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- scripts/clean-typescript-fluent-package.ps1
+++ scripts/clean-typescript-fluent-package.ps1
@@ -1,7 +1,9 @@
 param (
     [Parameter(Mandatory = $true)]
     [string]
-    $targetDirectory
+    $targetDirectory,
+    [string]
+    $packageName = "@microsoft/msgraph-sdk-javascript"
 )
 if ($targetDirectory -eq $null) {
     Write-Error "Target directory is required"
@@ -14,7 +16,7 @@ if (Test-Path .\models) {
 $fluentAPIFiles = Get-ChildItem -File -Filter *.ts -Recurse
 foreach($fluentAPIFile in $fluentAPIFiles) {
     $content = Get-Content $fluentAPIFile -raw
-    $content = $content -replace "from '[^']*/models/([^;]*)';", "from '@microsoft/msgraph-sdk-javascript/src/models/`$1';"
+    $content = $content -replace "from '[^']*/models/([^;]*)';", "from '$packageName/src/models/`$1';"
     Set-Content $fluentAPIFile -Value $content
 }
 Pop-Location
