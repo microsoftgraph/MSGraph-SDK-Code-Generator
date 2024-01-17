65d09c6ec7ac2974efa2052ae112bf888750709d
Vincent Biret <vibiret@microsoft.com>
2024-01-17 13:12:04 -0500

    - fixes import path
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- scripts/clean-typescript-fluent-package.ps1
+++ scripts/clean-typescript-fluent-package.ps1
@@ -16,7 +16,7 @@ if (Test-Path .\models) {
 $fluentAPIFiles = Get-ChildItem -File -Filter *.ts -Recurse
 foreach($fluentAPIFile in $fluentAPIFiles) {
     $content = Get-Content $fluentAPIFile -raw
-    $content = $content -replace "from '[^']*/models/([^;]*)';", "from '$packageName/src/models/`$1';"
+    $content = $content -replace "from '[^']*/models/([^;]*)';", "from '$packageName/models/`$1';"
     Set-Content $fluentAPIFile -Value $content
 }
 Pop-Location
