80f9bfa4fb33eeb79524ef463db9325fd538ca65
Vincent Biret <vibiret@microsoft.com>
2024-01-17 13:11:48 -0500

    - adds exception for the service client
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- scripts/clean-typescript-main-package.ps1
+++ scripts/clean-typescript-main-package.ps1
@@ -8,6 +8,6 @@ if ($targetDirectory -eq $null) {
     Exit 1
 }
 Push-Location $targetDirectory
-Remove-Item *.ts -Verbose -Exclude "index.ts"
+Remove-Item *.ts -Verbose -Exclude "index.ts", "graphServiceClient.ts"
 Get-ChildItem -Directory -Exclude models | ForEach-Object {Remove-Item -r $_.FullName}
 Pop-Location
