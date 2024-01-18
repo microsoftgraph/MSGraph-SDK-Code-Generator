95a5fb758d7ceb475318564d4bfd35a88830c902
Vincent Biret <vibiret@microsoft.com>
2024-01-18 14:04:02 -0500

    - disables hint mode for generation
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- scripts/generate-typescript.ps1
+++ scripts/generate-typescript.ps1
@@ -13,10 +13,12 @@ param (
     [string]
     $clientName = "GraphBaseServiceClient"
 )
+$ENV:KIOTA_TUTORIAL_ENABLED = $false
 $modelsPackageDirectoryName = $packageName.Split('/')[1]
 $rootSegments = Get-ChildItem -Directory -Exclude $modelsPackageDirectoryName -Path $targetDirectory | Select-Object -ExpandProperty Name | % { $_.Substring($modelsPackageDirectoryName.Length + 1)}
 foreach($rootSegment in $rootSegments) {
     $fluentAPIPackageDirectoryPath = "$targetDirectory/$modelsPackageDirectoryName-$rootSegment"
+    Write-Host "generating segment $rootSegment"
     Invoke-Expression "$kiotaPath generate -o $fluentAPIPackageDirectoryPath -d $descriptionPath -c $($rootSegment.Substring(0,1).ToUpper() + $rootSegment.Substring(1))ServiceClient -l TypeScript -n github.com/microsoftgraph/msgraph-sdk-typescript/ -e '/me' -e '/me/**' -i '/$rootSegment' -i '/$rootSegment/**'$additionalArguments"
     .\scripts\clean-typescript-fluent-package.ps1 -targetDirectory $fluentAPIPackageDirectoryPath -packageName $packageName
 }
