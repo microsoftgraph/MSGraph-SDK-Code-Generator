4562a6f24f9db196ff55d7a7604a0cfc16ab0bfd
Vincent Biret <vibiret@microsoft.com>
2024-01-18 13:20:15 -0500

    - fixes client name
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- scripts/generate-typescript.ps1
+++ scripts/generate-typescript.ps1
@@ -9,14 +9,16 @@ param (
     [string]
     $descriptionPath = "https://aka.ms/graph/v1.0/openapi.yaml",
     [string]
-    $additionalArguments = ""
+    $additionalArguments = "",
+    [string]
+    $clientName = "GraphBaseServiceClient"
 )
 $modelsPackageDirectoryName = $packageName.Split('/')[1]
 $rootSegments = Get-ChildItem -Directory -Exclude $modelsPackageDirectoryName -Path $targetDirectory | Select-Object -ExpandProperty Name | % { $_.Substring($modelsPackageDirectoryName.Length + 1)}
 foreach($rootSegment in $rootSegments) {
     $fluentAPIPackageDirectoryPath = "$targetDirectory/$modelsPackageDirectoryName-$rootSegment"
-    . "$kiotaPath generate -o $fluentAPIPackageDirectoryPath -d $descriptionPath -c $($rootSegment.Substring(0,1).ToUpper() + $rootSegment.Substring(1))ServiceClient -l TypeScript -n github.com/microsoftgraph/msgraph-sdk-typescript/ -i '/$rootSegment' -i '/$rootSegment/**'$additionalArguments"
+    . "$kiotaPath generate -o $fluentAPIPackageDirectoryPath -d $descriptionPath -c $($rootSegment.Substring(0,1).ToUpper() + $rootSegment.Substring(1))ServiceClient -l TypeScript -n github.com/microsoftgraph/msgraph-sdk-typescript/ -e '/me' -e '/me/**' -i '/$rootSegment' -i '/$rootSegment/**'$additionalArguments"
     .\scripts\clean-typescript-fluent-package.ps1 -targetDirectory $fluentAPIPackageDirectoryPath -packageName $packageName
 }
-. "$kiotaPath generate -o $fluentAPIPackageDirectoryPath -d $descriptionPath -c GraphServiceClient -l TypeScript -n github.com/microsoftgraph/msgraph-sdk-typescript/ $additionalArguments"
+. "$kiotaPath generate -o $fluentAPIPackageDirectoryPath -d $descriptionPath -c $clientName -l TypeScript -n github.com/microsoftgraph/msgraph-sdk-typescript/ -e '/me' -e '/me/**' $additionalArguments"
 .\scripts\clean-typescript-main-package.ps1 -targetDirectory $targetDirectory
