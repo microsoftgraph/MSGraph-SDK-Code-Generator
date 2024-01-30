6dcb68ddf23bc0777e2d98460a008514f5e2c2f4
Vincent Biret <vibiret@microsoft.com>
2024-01-30 09:51:51 -0500

    - excludes version from suppression
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- scripts/remove-typescript-fluent-api-from-main-package.ps1
+++ scripts/remove-typescript-fluent-api-from-main-package.ps1
@@ -4,6 +4,6 @@ param (
     $targetDirectory
 )
 Push-Location $targetDirectory
-Remove-Item *.ts -Verbose -Exclude "index.ts", "graphServiceClient.ts", "graphBetaServiceClient.ts", "graphRequestAdapter.ts", "graphBetaRequestAdapter.ts"
+Remove-Item *.ts -Verbose -Exclude "index.ts", "graphServiceClient.ts", "graphBetaServiceClient.ts", "graphRequestAdapter.ts", "graphBetaRequestAdapter.ts", "version.ts"
 Get-ChildItem -Directory -Exclude "models" | ForEach-Object {Remove-Item -r $_.FullName}
 Pop-Location
