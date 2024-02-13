b54b0429e865f635c7022ee1732b134055fd7fb9
ramsessanchez <63934382+ramsessanchez@users.noreply.github.com>
2024-02-12 17:00:44 -0800

    remove typewritter java from generation pipeline

--- scripts/clean-java-files.ps1
+++
@@ -1,7 +0,0 @@
-$modelsAndRequestsDirectories = Get-ChildItem $env:MainDirectory -Include models,requests -Recurse -Directory
-
-foreach ($directory in $modelsAndRequestsDirectories)
-{
-    Remove-Item $directory.FullName -Recurse -Force
-}
-Write-Host "Removed the existing generated files in the repo's main directory: $env:MainDirectory" -ForegroundColor Green
