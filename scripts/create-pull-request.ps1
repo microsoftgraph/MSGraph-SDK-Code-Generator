8255fc956c04796a679b942dccf0bde0bfa6ca6a
Andrew Omondi <andrew.omondi@microsoft.com>
2025-02-05 09:56:05 +0300

    ci: generate pull requests using Github application

--- scripts/create-pull-request.ps1
+++ scripts/create-pull-request.ps1
@@ -11,7 +11,7 @@ if (($env:GeneratePullRequest -eq $False)) { # Skip CI if manually running this 
 
 $version = $env:Version
 $title = "Generated $version models and request builders"
-$body = "This pull request was automatically created by Azure Pipelines. **Important** Check for unexpected deletions or changes in this PR."
+$body = ":bangbang:**_Important_**:bangbang: <br> Check for unexpected deletions or changes in this PR and ensure relevant CI checks are passing. <br><br> **Note:** This pull request was automatically created by Azure pipelines."
 $baseBranchParameter = ""
 
 if (![string]::IsNullOrEmpty($env:BaseBranch))
@@ -19,7 +19,12 @@ if (![string]::IsNullOrEmpty($env:BaseBranch))
     $baseBranchParameter = "-B $env:BaseBranch" # optionally pass the base branch if provided as the PR will target the default branch otherwise
 }
 
- # No need to specify reviewers as code owners should be added automatically.
+# The installed application is required to have the following permissions: read/write on pull requests/
+$tokenGenerationScript = "$env:ScriptsDirectory\Generate-Github-Token.ps1"
+$env:GITHUB_TOKEN = & $tokenGenerationScript -AppClientId $env:GhAppId -AppPrivateKeyContents $env:GhAppKey -Repository $env:RepoName
+Write-Host "Fetched Github Token for PR generation and set as environment variable." -ForegroundColor Green
+
+# No need to specify reviewers as code owners should be added automatically.
 Invoke-Expression "gh auth login" # login to GitHub
 Invoke-Expression "gh pr create -t ""$title"" -b ""$body"" $baseBranchParameter | Write-Host"
 
