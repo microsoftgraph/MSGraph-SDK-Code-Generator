64e43e325ea0da1f204f76f90c76dc83dcafd88c
Musale Martin <martinmusale@microsoft.com>
2025-02-28 17:44:41 +0300

    Update typescript to copy models

---
+++ scripts/copy-ccs-typescript-sdk-models.ps1
@@ -0,0 +1,8 @@
+Write-Host "Path of new models: $env:OutputFullPath"
+Write-Host "Path to repo models directory: $env:RepoModelsDir"
+ 
+#copy new modes
+# Create a dest folder explicitly https://github.com/PowerShell/PowerShell/issues/13352#issuecomment-669025179
+New-Item -ItemType directory -Path $env:RepoModelsDir -Force
+Move-Item $env:OutputFullPath $env:RepoModelsDir -Force
+Write-Host "Moved the models from $env:OutputFullPath into the local repo $env:RepoModelsDir." -ForegroundColor Green
