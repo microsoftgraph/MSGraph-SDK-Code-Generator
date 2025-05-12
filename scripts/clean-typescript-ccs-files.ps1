9dd962a169273724dc7e767b64b98f10a9cdc2fb
Musale Martin <martinmusale@microsoft.com>
2025-05-12 16:52:09 +0300

    Enhance cleaning script to remove the correct ts files

--- scripts/clean-typescript-ccs-files.ps1
+++ scripts/clean-typescript-ccs-files.ps1
@@ -16,7 +16,11 @@ $folderToCleanPath = Join-Path $targetDirectory -ChildPath $folderToClean
 if (Test-Path $folderToCleanPath) {
     Write-Host "Cleaning contents in: $folderToClean" -ForegroundColor Cyan
     Push-Location $folderToCleanPath
+    # Clean directories
     Get-ChildItem -Directory | ForEach-Object { Remove-Item -r $_.FullName }
+    # Clean TypeScript files except index.ts and *ServiceClient.ts
+    Remove-Item *.ts -Exclude "index.ts", "*ServiceClient.ts"
+    # Remove kiota lock file
     Remove-Item $kiotaLockFileName -ErrorAction SilentlyContinue -Verbose
     Pop-Location
     Write-Host "Successfully cleaned contents in: $folderToClean" -ForegroundColor Green
